0dbecb8d71345422617aef49f9dfbf057be07e2a
David <david_soderberg@outlook.com>
2020-07-13 21:58:00 +0200

    copy from another repo and docker setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,8 @@ module.exports = {
     ecmaVersion: 11,
   },
   rules: {
+    'global-require': 0,
+    'no-underscore-dangle': 0,
+    'no-param-reassign': 0,
   },
 };
--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-console.log('Hello world v2');
+if (process.env === 'test') require('dotenv').config();
+
+const mongoose = require('mongoose');
+const avanza = require('./lib/avanza');
+const service = require('./lib/service');
+
+const credentials = {
+  username: process.env.AVANZA_USERNAME,
+  password: process.env.AVANZA_PASSWORD,
+  totpSecret: process.env.AVANZA_TOTP_SECRET,
+};
+
+const DB_URL = 'mongodb://mongo:27017/';
+
+mongoose.connect(DB_URL,
+  { useUnifiedTopology: true, useNewUrlParser: true, dbName: process.env.DB_NAME })
+  .then(() => {
+    console.log('Database connection established...');
+    avanza.authenticate(credentials)
+      .then(() => {
+        console.log('Authenticated to Avanza...');
+        console.log('Service starting...');
+        service.start(avanza);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
