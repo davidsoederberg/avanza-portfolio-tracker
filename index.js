e84225fc4192c4087f9ed839f93f54e84ed05d2d
David <david_soderberg@outlook.com>
2020-07-14 18:59:29 +0200

    init fixes (refactor needed)

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ if (process.env === 'test') require('dotenv').config();
 const mongoose = require('mongoose');
 const avanza = require('./lib/avanza');
 const service = require('./lib/service');
+const pC = require('./lib/api/portfolioController');
 
 const credentials = {
   username: process.env.AVANZA_USERNAME,
@@ -10,7 +11,7 @@ const credentials = {
   totpSecret: process.env.AVANZA_TOTP_SECRET,
 };
 
-const DB_URL = 'mongodb://mongo:27017/';
+const DB_URL = 'mongodb://mongo:27017';
 
 mongoose.connect(DB_URL,
   { useUnifiedTopology: true, useNewUrlParser: true, dbName: process.env.DB_NAME })
@@ -19,8 +20,18 @@ mongoose.connect(DB_URL,
     avanza.authenticate(credentials)
       .then(() => {
         console.log('Authenticated to Avanza...');
-        console.log('Service starting...');
-        service.start(avanza);
+        pC.check()
+          .then((res) => {
+            if (!res) {
+              console.log('Init portfolio...');
+              pC.init();
+            }
+            console.log('Service starting...');
+            setTimeout(() => { service.start(avanza); }, 3000);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
       })
       .catch((err) => {
         console.log(err);
