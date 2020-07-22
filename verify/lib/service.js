a0ca769fdf84f63f72c87acdb1b37b83994467eb
David <david_soderberg@outlook.com>
2020-07-22 16:55:41 +0200

    fix seconds

--- verify/lib/service.js
+++ verify/lib/service.js
@@ -10,11 +10,11 @@ exports.start = () => {
   vC.checkOrInitNewDay();
 
   // INTRADAY
-  new CronJob('5 * 9-16 * * 1-5', () => vC.verifyDataPoint(), null, true, 'Europe/Stockholm');
+  new CronJob('30 * 9-16 * * 1-5', () => vC.verifyDataPoint(), null, true, 'Europe/Stockholm');
   // ENDING HOUR
-  new CronJob('5 0-30 17 * * 1-5', () => vC.verifyDataPoint(), null, true, 'Europe/Stockholm');
+  new CronJob('30 0-30 17 * * 1-5', () => vC.verifyDataPoint(), null, true, 'Europe/Stockholm');
   // LAST UPDATE
-  // new CronJob('5 0 22 * * 1-5', () => verify(), null, true, 'Europe/Stockholm');
+  // new CronJob('30 0 22 * * 1-5', () => verify(), null, true, 'Europe/Stockholm');
   // INIT NEW INTRADAY
-  new CronJob('5 0 6 * * 1-5', () => vC.checkOrInitNewDay(), null, true, 'Europe/Stockholm');
+  new CronJob('30 0 6 * * 1-5', () => vC.checkOrInitNewDay(), null, true, 'Europe/Stockholm');
 };
