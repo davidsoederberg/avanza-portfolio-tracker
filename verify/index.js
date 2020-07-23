d2bb8497f4ae43981a74562751f7d123a5686a9a
David Söderberg <31454554+davidsoederberg@users.noreply.github.com>
2020-07-23 15:54:38 +0200

    Verify init (#8)
    * Verify init

--- verify/index.js
+++ verify/index.js
@@ -2,13 +2,27 @@ if (process.env === 'test') require('dotenv').config();
 
 const mongoose = require('mongoose');
 const service = require('./lib/service');
+const vC = require('./lib/api/verifyController');
 
 const DB_URL = 'mongodb://mongo:27017';
+let waitTime = 0;
 
 mongoose.connect(DB_URL,
   { useUnifiedTopology: true, useNewUrlParser: true, dbName: process.env.DB_NAME })
   .then(() => {
-    service.start();
+    vC.verifyExist()
+      .then((res) => {
+        if (!res) {
+          waitTime = 3000;
+          console.log('Init verify...');
+          vC.init();
+        }
+        console.log('Service starting...');
+        setTimeout(() => { service.start(); }, waitTime);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   })
   .catch((err) => {
     console.log(err);
